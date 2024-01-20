be37aeda3ea4d127e4b041613d9c3ec5c84f912a
Mikhail Melnikov <144221975+Zeems111@users.noreply.github.com>
2024-01-21 01:32:01 +0300

    Searching for minimum in a sliding window

---
+++ Structures and algorithms/Sliding_window_minimum.py
@@ -0,0 +1,29 @@
+"""
+Given an array a, a sliding window of size k is moving
+from the very left of the array to the very right.
+Each time the sliding window moves right by one position.
+For each sliding window position return minimum value
+of all numbers inside the sliding window.
+
+"""
+
+
+def sliding_window_min(a, k):
+    q = []
+    shadow_q = []
+    for i in range(len(a)):
+        if q:
+            if q[0] < i - k + 1:
+                del q[0]
+            while q and a[i] <= a[q[-1]]:
+                q.pop()
+        q.append(i)
+        if i >= k-1:
+            print("min:", a[q[0]], "\twindow:", a[i-k+1:i+1])
+
+if __name__ == '__main__':
+    _, k = input().split()
+    k = int(k)
+
+    arr = list(map(int, input().split()))
+    sliding_window_min(arr, k)
